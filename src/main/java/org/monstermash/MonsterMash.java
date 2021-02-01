041cfec12c3cf634e1f21df29048d1584d780cfc
Ondrej Janus <janusonx@gmail.com>
2021-02-01 19:18:04 +0100

    Add menu

--- src/main/java/org/monstermash/MonsterMash.java
+++ src/main/java/org/monstermash/MonsterMash.java
@@ -1,13 +1,16 @@
 package org.monstermash;
 
 import org.monstermash.ui.MainWindow;
+import org.monstermash.ui.MenuBar;
 import org.monstermash.ui.MonsterOptions;
 
 import javax.swing.SwingUtilities;
 
 public class MonsterMash {
     public static void main(String[] args) {
-        MainWindow mainWindow = new MainWindow(new MonsterOptions());
+        MonsterOptions monsterOptions = new MonsterOptions();
+        MenuBar menuBar = new MenuBar();
+        MainWindow mainWindow = new MainWindow(monsterOptions, menuBar);
         SwingUtilities.invokeLater(mainWindow::run);
     }
 }
--- src/main/java/org/monstermash/ui/MainWindow.java
+++ src/main/java/org/monstermash/ui/MainWindow.java
@@ -7,16 +7,20 @@ import java.awt.Dimension;
 
 public class MainWindow extends JFrame {
     private final MonsterOptions monsterOptions;
+    private final MenuBar menu;
 
-    public MainWindow(MonsterOptions monsterOptions) {
+    public MainWindow(MonsterOptions monsterOptions, MenuBar menu) {
         super("MonsterMash!");
         this.monsterOptions = monsterOptions;
+        this.menu = menu;
     }
 
     public void run() {
         this.monsterOptions.init();
         super.setLayout(new BorderLayout());
         super.add(monsterOptions, BorderLayout.NORTH);
+        super.add(menu);
+        super.setJMenuBar(menu);
         super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         super.setPreferredSize(new Dimension(850, 615));
         super.setMinimumSize(super.getPreferredSize());
